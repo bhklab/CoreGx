041b8c0241f6ba6fcdc31edf73e4006bdf140d75
markcfreeman <marky.freeman@gmail.com>
2017-07-25 13:40:35 -0400

    reformat reformatData

--- R/reformatData.R
+++ R/reformatData.R
@@ -2,71 +2,78 @@
   if (!(is.logical(x_to_log))) {
     stop("x_to_log must be a logical.")
   }
-
+  
   if (!(is.logical(y_to_log))) {
     stop("y_to_log must be a logical.")
   }
-
+  
   if (!(is.logical(y_to_frac))) {
     stop("y_to_frac must be a logical.")
   }
-
-  if (any(is.na(x) & (!is.na(y)))){
-    warning("Missing x-values with non-missing y-values encountered. Removed y-values correspoding to those x-values.")
-    myx <- !is.na(x)
-    x <- as.numeric(x[myx])
-    y <- as.numeric(y[myx])
-  }
-
-  if (any((!is.na(x)) & is.na(y))){
-    warning("Missing y-values with non-missing x-values encountered. Removed x values correspoding to those y-values.")
-    myy <- !is.na(y)
-    x <- as.numeric(x[myy])
-    y <- as.numeric(y[myy])
-  }
-
-  if (is.unsorted(x)){
-    warning("x-values were unsorted. Sorting x-values and ordering y-values correspondingly.")
-    myx <- order(x)
-    x <- x[myx]
-    y <- y[myx]
-  }
-
+  
   if (x_to_log) {
-    ii <- which(x == 0)
-    if(length(ii) > 0) {
-      x <- x[-ii]
-      y <- y[-ii]
-    }
-    log_x <- log10(x)
-    if (!missing(pars) && length(pars) == 3) {
-      pars[[3]] <- log10(pars[[3]])
-    }
-  }
-
-  if (y_to_frac) {
-    y <- y / 100
-    if (!missing(pars) && length(pars) == 3) {
-      pars[[2]] <- pars[[2]] / 100
-    }
-  }
-
-  if (trunc) {
-    y = pmin(as.numeric(y), 1)
-    y = pmax(as.numeric(y), 0)
-  }
-
-  if (y_to_log) {
-    log_y <- log10(y)
+    x <- log10(x)
   }
   
-  if (missing(pars)) {
-    return(list("x" = x,
-                "y" = y))
-  } else if (missing(y)) {
-    return(list("x" = x,
-                "pars" = pars))
-  } else {
-    stop("y and pars should not both be missing!")
+  if (is.unsorted(x)) {
+    warning("x-values passed in unsorted. Sorting x-values and corresponding y-values (if passed in).")
+    xOrder <- order(x)
+    x <- x[xOrder]
+  }
+  
+  if (!missing(y)) {
+    if (any(is.na(x) & (!is.na(y)))){
+      warning("Missing x-values with non-missing y-values encountered. Removed y-values correspoding to those x-values.")
+      myx <- !is.na(x)
+      x <- as.numeric(x[myx])
+      y <- as.numeric(y[myx])
+    }
+    
+    if (any((!is.na(x)) & is.na(y))){
+      warning("Missing y-values with non-missing x-values encountered. Removed x values correspoding to those y-values.")
+      myy <- !is.na(y)
+      x <- as.numeric(x[myy])
+      y <- as.numeric(y[myy])
+    }
+    
+    if (is.unsorted(x)){
+      y <- y[xOrder]
+    }
+    
+    if (trunc) {
+      y = pmin(as.numeric(y), 1)
+      y = pmax(as.numeric(y), 0)
+    }
+    
+    if (x_to_log) {
+      x0s <- which(x == -Inf)
+      if(length(x0s) > 0) {
+        x <- x[-x0s]
+        y <- y[-x0s]
+      }
+    }
+    
+    if (y_to_log) {
+      y <- log10(y)
+    }
+    
+    if (y_to_frac) {
+      y <- y / 100
+    }
+    
+    return(list("x" = x, "y" = y))
+  }
+  
+  if (!missing(pars)) {
+    
+    if (x_to_log && length(pars) == 3) {
+      pars[[3]] <- log10(pars[[3]])
+    }
+    
+    if (y_to_frac && length(pars) == 3) {
+      pars[[2]] <- pars[[2]] / 100
+    }
+    
+    return(list("x" = x, "pars" = pars))
   }
 }
