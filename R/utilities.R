ec4712bf8f25bec2f40b6a7b96b815a5d49178d4
ChristopherEeles <christopher.eeles@outlook.com>
2020-04-21 15:45:58 -0400

    Update: Forgot to change  to ; now fixed

--- R/utilities.R
+++ R/utilities.R
@@ -56,67 +56,67 @@
     
     if (!is.logical(x_as_log)) {
         if (verbose == 2) {
-            print("x_as_log:")
-            print(x_as_log)
+            message("x_as_log:")
+            message(x_as_log)
         }
         stop("'x_as_log' is not a logical.")
     }
     
     if (!is.logical(y_as_log)) {
         if (verbose == 2) {
-            print("y_as_log:")
-            print(y_as_log)
+            message("y_as_log:")
+            message(y_as_log)
         }
         stop("'y_as_log' is not a logical.")
     }
     
     if (!is.logical(y_as_pct)) {
         if (verbose == 2) {
-            print("y_as_pct:")
-            print(y_as_pct)
+            message("y_as_pct:")
+            message(y_as_pct)
         }
         stop("'y_as_pct' is not a logical.")
     }
     
     if (!is.logical(trunc)) {
         if (verbose == 2) {
-            print("trunc:")
-            print(trunc)
+            message("trunc:")
+            message(trunc)
         }
         stop("'trunc' is not a logical.")
     }
     
     if (y_as_pct && y_as_log) {
         if (verbose == 2) {
-            print("y_as_pct:")
-            print(y_as_pct)
-            print("y_as_log:")
-            print(y_as_log)
+            message("y_as_pct:")
+            message(y_as_pct)
+            message("y_as_log:")
+            message(y_as_log)
         }
         warning("y_as_pct and y_as_log flags should almost certainly not both be TRUE.")
     }
     
     if (!(verbose %in% c(0, 1, 2))) {
-        print("verbose:")  #can't have the if(verbose == 2) statement here since verbose itself is the problem!
-        print(verbose)
+        message("verbose:")  #can't have the if(verbose == 2) statement here since verbose itself is the problem!
+        message(verbose)
         stop("'verbose' flag is not set correctly.")
     }
     
     if (!missing(x)) {
         if (!all(is.finite(x) || is.na(x) || (x_as_log && x == -Inf))) {
             if (verbose == 2) {
-                print("x:")
-                print(x)
+                message("x:")
+                message(x)
             }
             stop("x must contain only real numbers, NA-values, and/or -Inf (if x_as_log flag is set to TRUE).")
         }
         
         if (x_as_log == FALSE && min(x) < 0) {
             if (verbose == 2) {
-                print("x:")
-                print(x)
-                print("x_as_log:")
-                print(x_as_log)
+                message("x:")
+                message(x)
+                message("x_as_log:")
+                message(x_as_log)
             }
             stop("Negative x-values encountered. Data may be inappropriate, or 'x_as_log' flag may be set incorrectly.")
         }
@@ -133,8 +133,8 @@
             if (pars[[1]] < 0 || pars[[2]] < 0) {
                 # HS or alpha
                 if (verbose == 2) {
-                  print("pars:")
-                  print(pars)
+                  message("pars:")
+                  message(pars)
                 }
                 warning("Curve parameters may be inappropriately set to negative values.")
             }
@@ -142,20 +142,20 @@
             if (length(pars) == 3) {
                 # and thus we are in PharmacoGx
                 if (x_as_log == FALSE && pars[[3]] < 0) {
-                  print("pars:")
-                  print(pars[[3]])
-                  print("x_as_log:")
-                  print(x_as_log)
+                  message("pars:")
+                  message(pars[[3]])
+                  message("x_as_log:")
+                  message(x_as_log)
                   stop("'x_as_log' flag may be set incorrectly, as the EC50 is negative when a positive value is expected.")
                 }
                 
                 if (y_as_pct == FALSE) {
                   if (pars[[2]] > 1) {
                     if (verbose == 2) {
-                      print("pars:")
-                      print(pars[[2]])
-                      print("y_as_pct:")
-                      print(y_as_pct)
+                      message("pars:")
+                      message(pars[[2]])
+                      message("y_as_pct:")
+                      message(y_as_pct)
                     }
                     warning("Warning: 'y_as_pct' flag may be set incorrectly.")
                   }
@@ -165,8 +165,8 @@
                   if (verbose) {
                     warning("Alpha is greater than beta.")
                     if (verbose == 2) {
-                      print("pars:")
-                      print(pars)
+                      message("pars:")
+                      message(pars)
                     }
                   }
                 }
@@ -179,8 +179,8 @@
         
         if (!all(is.finite(y) || is.na(y) || (y_as_log && y == -Inf))) {
             if (verbose == 2) {
-                print("y:")
-                print(y)
+                message("y:")
+                message(y)
             }
             stop("y must contain only real numbers, NA-values, and/or -Inf (if y_as_log is set to TRUE).")
         }
@@ -189,8 +189,8 @@
             if (verbose) {
                 warning("Warning: Negative y data.")
                 if (verbose == 2) {
-                  print("y:")
-                  print(y)
+                  message("y:")
+                  message(y)
                 }
             }
         }
@@ -199,8 +199,8 @@
             if (verbose) {
                 warning("Warning: y data exceeds negative control.")
                 if (verbose == 2) {
-                  print("y:")
-                  print(y)
+                  message("y:")
+                  message(y)
                 }
             }
         }
@@ -211,10 +211,10 @@
                 if (verbose) {
                   warning("Negative y-values encountered. y data may be inappropriate, or 'y_as_log' flag may be set incorrectly.")
                   if (verbose == 2) {
-                    print("y:")
-                    print(y)
-                    print("y_as_log:")
-                    print(y_as_log)
+                    message("y:")
+                    message(y)
+                    message("y_as_log:")
+                    message(y_as_log)
                   }
                 }
             }
@@ -223,10 +223,10 @@
                 if (verbose) {
                   warning("Warning: 'y_as_pct' flag may be set incorrectly.")
                   if (verbose == 2) {
-                    print("y:")
-                    print(y)
-                    print("y_as_pct:")
-                    print(y_as_pct)
+                    message("y:")
+                    message(y)
+                    message("y_as_pct:")
+                    message(y_as_pct)
                   }
                 }
             }
@@ -235,20 +235,20 @@
                 if (verbose) {
                   warning("Warning: 'y_as_pct' flag may be set incorrectly.")
                   if (verbose == 2) {
-                    print("y:")
-                    print(y)
-                    print("y_as_pct:")
-                    print(y_as_pct)
+                    message("y:")
+                    message(y)
+                    message("y_as_pct:")
+                    message(y_as_pct)
                   }
                 }
             }
             
             if (!missing(x) && length(x) != length(y)) {
                 if (verbose == 2) {
-                  print("x:")
-                  print(x)
-                  print("y:")
-                  print(y)
+                  message("x:")
+                  message(x)
+                  message("y:")
+                  message(y)
                 }
                 stop("Vector of x-values is not of same length as vector of y-values.")
             }
@@ -261,42 +261,42 @@
     if (!missing(lower) && !missing(upper)) {
         if (!(is.double(lower))) {
             if (verbose == 2) {
-                print("lower:")
-                print(lower)
+                message("lower:")
+                message(lower)
             }
             stop("The lower bound must be a positive real number.")
         }
         
         if (!(is.double(lower))) {
             if (verbose == 2) {
-                print("upper:")
-                print(upper)
+                message("upper:")
+                message(upper)
             }
             stop("The upper bound must be a positive real number.")
         }
         
         if (lower >= upper) {
             if (verbose == 2) {
-                print("lower:")
-                print(lower)
-                print("upper:")
-                print(upper)
+                message("lower:")
+                message(lower)
+                message("upper:")
+                message(upper)
             }
             stop("The lower bound of the range of allowed x-values must be less than the upper bound.")
         }
         
         if (lower < 0) {
             if (verbose == 2) {
-                print("lower:")
-                print(lower)
+                message("lower:")
+                message(lower)
             }
             stop("The lower bound of the range of allowed x-values must be nonnegative.")
         }
         
         if (upper < 0) {
             if (verbose == 2) {
-                print("upper:")
-                print(upper)
+                message("upper:")
+                message(upper)
             }
             stop("The upper bound of the range of allowed x-values must be nonnegative.")
         }
