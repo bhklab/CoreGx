a8199226fbbc87cd148f143759d0598cbd18da93
Petr Smirnov <psmirnov2000@gmail.com>
2020-06-25 17:05:00 -0400

    added generics file

---
+++ R/generics.R
@@ -0,0 +1,41 @@
+
+#' 
+#' @export
+setGeneric("subsetTo", function(object, ...) standardGeneric("subsetTo"))
+
+
+
+#' 
+#' @export
+setGeneric("computeAUC", function(object, ...) standardGeneric("computeAUC"))
+
+
+
+#' 
+#' @export
+setGeneric("summarizeSensitivityProfiles", function(object, ...) standardGeneric("summarizeSensitivityProfiles"))
+
+
+
+#' 
+#' @export
+setGeneric("summarizeMolecularProfiles", function(object, ...) standardGeneric("summarizeMolecularProfiles"))
+
+
+
+#' 
+#' @export
+setGeneric("showSigAnnot", function(object, ...) standardGeneric("showSigAnnot"))
+
+
+
+#' 
+#' @export
+setGeneric("drugSensitivitySig", function(object, ...) standardGeneric("drugSensitivitySig"))
+
+
+
+#' 
+#' @export
+setGeneric(".resaveAllExampleDatasets", function(object, ...) standardGeneric(".resaveAllExampleDatasets"))
+
