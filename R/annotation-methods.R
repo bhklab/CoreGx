79fbc317af6468d22c53ec94bdbee47ad73b2bde
ChristopherEeles <christopher.eeles@outlook.com>
2020-06-30 13:56:37 -0400

    Update: Added `annotation` import from BiocGenerics instead of defining my own generic * Passing BiocCheck with only notes

---
+++ R/annotation-methods.R
@@ -0,0 +1,38 @@
+#' annotation Slot Getter
+#'
+#' @param object A \code{RadioSet}
+#'
+#' @return A \code{list} of named annotaiton
+#'
+#' @examples
+#' data(clevelandSmall_cSet)
+#' annotation(clevelandSmall_cSet)
+#'
+#' @describeIn CoreSet Retrieve the annotations slot form an rSet
+#'
+#' @importMethodsFrom BiocGenerics annotation
+#' @export
+setMethod('annotation', signature("CoreSet"), function(object) {
+    object@annotation
+})
+
+#' annotation<- Slot Setter
+#'
+#' @param object A \code{RadioSet}
+#' @param value A \code{list} of annotations to add to the annotatiosn slot of
+#'   an rSet
+#'
+#' @return A copy of the \code{CoreSet} with the updated annotation slot
+#'
+#' @examples
+#' data(clevelandSmall_cSet)
+#' annotation(clevelandSmall_cSet) <- annotation(clevelandSmall_cSet)
+#'
+#' @describeIn RadioSet Update the annotation slot of a tSet
+#'
+#' @importMethodsFrom BiocGenerics annotation<-
+#' @export
+setReplaceMethod("annotation", signature("CoreSet", "list"), function(object, value) {
+    object@annotation <- value
+    object
+})
