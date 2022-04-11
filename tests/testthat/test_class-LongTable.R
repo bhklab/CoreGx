9ab142b3841f215cc64fa6a7070be7ff3bd7b3ac
ChristopherEeles <christopher.eeles@outlook.com>
2022-04-11 13:31:31 -0400

    Add: test that LongTable constructor returns same indices as reindex on a new table; reindex is now ~0.5s when no work needs to be done

--- tests/testthat/test_class-LongTable.R
+++ tests/testthat/test_class-LongTable.R
@@ -10,7 +10,29 @@ test_that("`rowData,LongTable-method` orders data correctly", {
 
 })
 
+testthat::test_that("`assay,LongTable-method` and `assays,LongTable-method` return equivalent data", {
+    assay_list <- lapply(seq_along(assayNames(lt)), FUN=assay,
+        x=lt, withDimnames=TRUE)
+    assays_ <- assays(lt)
+    for (i in seq_along(assay_list)) {
+        print(i)
+        testthat::expect_true(all.equal(assay_list[[i]], assays_[[i]]))
+    }
+})
+
 # ==== LongTable-utils.R
+
+testthat::test_that("`reindex,LongTale-method` does not mutate by reference", {
+    .lt <- copy(lt)
+    nlt <- reindex(lt)
+    testthat::expect_true(all.equal(.lt, lt))
+})
+
+testthat::test_that("`reindex,LongTable-method` has same index as LongTable constructor", {
+    nlt <- reindex(lt)
+    testthat::expect_true(all.equal(getIntern(nlt, "assayIndex"), getIntern(lt, "assayIndex")))
+    testthat::expect_true(all.equal(assays(nlt, raw=TRUE), assays(lt, raw=TRUE)))
+})
 
 testthat::test_that("`reindex,LongTable-method` does not corrupt data relationships", {
     nlt <- reindex(lt)
@@ -27,21 +49,5 @@ testthat::test_that("`reindex,LongTable-method` does not corrupt data relationsh
     for (i in seq_along(assayL1)) {
         print(i)
         testthat::expect_true(all.equal(assayL1[[i]], assayL2[[i]]))
-    }
-})
-
-testthat::test_that("`reindex,LongTale-method` does not mutate by reference", {
-    .lt <- copy(lt)
-    nlt <- reindex(lt)
-    testthat::expect_true(all.equal(.lt, lt))
-})
-
-testthat::test_that("`assay,LongTable-method` and `assays,LongTable-method` return equivalent data", {
-    assay_list <- lapply(seq_along(assayNames(lt)), FUN=assay,
-        x=lt, withDimnames=TRUE)
-    assays_ <- assays(lt)
-    for (i in seq_along(assay_list)) {
-        print(i)
-        testthat::expect_true(all.equal(assay_list[[i]], assays_[[i]]))
     }
 })
