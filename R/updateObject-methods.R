4f7afa9494e45baf66fa42b3b389791bfab9c28e
Christopher Eeles <christopher.eeles@outlook.com>
2022-04-20 22:00:17 -0400

    Handling drug -> treatment rename inside of updateObject method

--- R/updateObject-methods.R
+++ R/updateObject-methods.R
@@ -23,7 +23,7 @@ setMethod('updateObject', signature(object="CoreSet"),
         if (.hasSlot(object, "drug")) {
             treatment <- object@drug
         } else if (.hasSlot(Object, "radiation")) {
-            treatment <- data.frame()
+            treatment <- object@radiation
         } else  {
             treatment <- data.frame()
         }
@@ -51,17 +51,26 @@ setMethod('updateObject', signature(object="CoreSet"),
     } else {
         colnames(treatmentResponse$info) <- gsub("cellid", "sampleid",
             colnames(treatmentResponse$info))
+        colnames(treatmentResponse$info) <- gsub("drugid|radiationid",
+            "treatmentid", colnames(treatmentResponse$info))
     }
 
     mProf <- object@molecularProfiles
     for (i in seq_along(mProf)) {
         colnames(colData(mProf[[i]])) <- gsub("cellid", "sampleid",
             colnames(colData(mProf[[i]])))
+        colnames(colData(mProf[[i]])) <- gsub("drugid", "treatmentid",
+            colnames(colData(mProf[[i]])))
     }
     curation_ <- object@curation
     names(curation_) <- gsub("cell", "sample", names(curation_))
+    names(curation_) <- gsub("drug", "treatment", names(curation_))
     colnames(curation_$sample) <- gsub("cellid", "sampleid",
         colnames(curation_$sample))
+    if ("treatment" %in% names(curation_)) {
+        colnames(curation_$treatment) <- gsub("drugid|radiationid",
+            "treatmentid", colnames(curation_$treatment))
+    }
 
     ## TODO:: change any occurance of cellid to sample id in the old sensitivity
     ## slot list
