b5120758e69579e6bfcc4676342f12cf8e7fffa1
Christopher Eeles <christopher.eeles@outlook.com>
2022-05-31 19:13:08 -0400

    Add basic function documentation for aggregate2 function

--- R/aggregate-methods.R
+++ R/aggregate-methods.R
@@ -1,3 +1,37 @@
+library(CoreGx)
+library(PharmacoGx)
+library(data.table)
+library(BiocParallel)
+
+#' Functional API for data.table aggregation and capture of associated aggregate
+#' calls
+#'
+#' @param x `data.table`
+#' @param by `character` One or more valid column names in `x`.
+#' @param ... `call` One or more aggregations to compute for each by in x. If you
+#'   name items, that will be the column name of the associated column otherwise
+#'   a default name will be parsed from the function and column name.
+#' @param nthread `numeric(1)` Number of threads to use for split-apply-combine
+#'   parallelization. Uses `BiocParllel::bplapply` if nthread > 1. Does not
+#'   modify data.table threads, so be sure to use setDTthreads for reasonable
+#'   nested parallelism.
+#' @param BPPARAM `BiocParallelParam` object. Use to customized the
+#'   the parallization back-end of bplapply. Note, nthread over-rides any
+#'   settings from BPPARAM. For now, a progress bar is always used.
+#'
+#' @details
+#' Arguments in ... are substituted and wrapped in a list, which is passed
+#' through to the j argument of `[.data.table` interanally. The functin currently
+#' tries to build informative column names for unnamed arguments in ... by appending
+#' the name of the call with the name of its first argument, which is assumed to
+#' be the column name being aggregated over. If an argument to ... is named,
+#' that will be the column name for the column for that aggregation.
+#'
+#' @return `data.table` of aggregation results.
+#'
+#' @importFrom BiocParallel bpparam bpworkers bpprogressbar bplapply
+#' @importFrom data.table split rbindlist
+#' @export
 aggregate2 <- function(x, by, ..., nthread=1, BPPARAM=BiocParallel::bpparam()) {
     stopifnot(is.data.table(x))
     stopifnot(is.character(by) && all(by %in% colnames(x)))
@@ -13,6 +47,7 @@ aggregate2 <- function(x, by, ..., nthread=1, BPPARAM=BiocParallel::bpparam()) {
     }
     names(agg_call)[2L:length(agg_call)] <- dot_names
 
+    # -- compute the aggregates, parallelizing if nthread > 1
     if (nthread == 1) {
         res <- x[, eval(agg_call), by=c(by)]
     } else {
@@ -32,7 +67,8 @@ aggregate2 <- function(x, by, ..., nthread=1, BPPARAM=BiocParallel::bpparam()) {
 
 
 if (Sys.frame() == 0) {
-    debug(aggregate2)
+    sens <- fread(file.path(".local_data", "sensitivity_assay.csv"))
+    # debug(aggregate2)
     sens |>
         aggregate2(
             mv=mean(viability), mean(drug1dose),
@@ -40,7 +76,7 @@ if (Sys.frame() == 0) {
             nthread=10
         )
 
-    sens[is.na(drug2dose)] |>
+    sens[is.na(drug2dose)][drug1id %in% unique(drug1id)[1:5]] |>
         aggregate2(
             auc=tryCatch(computeAUC(drug1dose, viability), error=\(e) NA),
             by=c("drug1id", "cellid")
